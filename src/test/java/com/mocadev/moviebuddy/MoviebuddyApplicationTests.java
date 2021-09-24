f4027c2ddf57e9bafc4beb27e745b184d7b548f0
tony <chcjswo@gmail.com>
2021-09-24 23:49:00 +0900

    Initial commit

---
+++ src/test/java/com/mocadev/moviebuddy/MoviebuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocadev.moviebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
