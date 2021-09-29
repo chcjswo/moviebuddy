67cf4b42a66fc7e897bd567a4b8bae9fc5628cf3
tony <chcjswo@gmail.com>
2021-09-29 23:23:42 +0900

    Junit5 적용

--- src/test/java/com/mocadev/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/com/mocadev/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,6 +1,8 @@
 package com.mocadev.moviebuddy.domain;
 
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author chcjswo
@@ -9,15 +11,16 @@ import java.util.List;
  * @github https://github.com/chcjswo
  * @since 2021-09-29
  **/
-public class JaxbMovieReaderTest {
+class JaxbMovieReaderTest {
 
-	public static void main(String[] args) {
+	@Test
+	void notEmpty_LoadMovies() {
 		JaxbMovieReader jaxbMovieReader = new JaxbMovieReader();
 		final List<Movie> movies = jaxbMovieReader.loadMovies();
 		for (Movie movie : movies) {
 			System.out.println("movie = " + movie);
 		}
-		System.out.println("movies.size() = " + movies.size());
+		Assertions.assertEquals(1375, movies.size());
 	}
 
 }
