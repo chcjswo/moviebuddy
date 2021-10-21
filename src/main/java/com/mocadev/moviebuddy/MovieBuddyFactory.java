91385c7152aa8ebabe8f5aa79867483b501eb663
tony <chcjswo@gmail.com>
2021-10-21 23:48:28 +0900

    프로퍼티 파일 Environment를 사용해서 읽기

--- src/main/java/com/mocadev/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/com/mocadev/moviebuddy/MovieBuddyFactory.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
@@ -41,12 +42,18 @@ public class MovieBuddyFactory {
 	@Configuration
 	 static class DataSourceModuleConfig {
 
+		private final Environment env;
+
+		public DataSourceModuleConfig(Environment env) {
+			this.env = env;
+		}
+
 		@Profile(MovieBuddyProfile.CSV_MODE)
 		@Bean
 		public CsvMovieReader csvMovieReader() {
 			CsvMovieReader movieReader = new CsvMovieReader();
 
-			movieReader.setMetaData(System.getProperty("movie_metadata"));
+			movieReader.setMetaData(env.getProperty("movie_metadata"));
 			return movieReader;
 		}
 
@@ -54,7 +61,7 @@ public class MovieBuddyFactory {
 		@Bean
 		public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
 			XmlMovieReader movieReader = new XmlMovieReader(unmarshaller);
-			movieReader.setMetaData(System.getProperty("movie_metadata"));
+			movieReader.setMetaData(env.getProperty("movie_metadata"));
 			return movieReader;
 		}
 
